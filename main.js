0ad8a9bd259f2cecd2ce1be898544c695d1fd6c7
mckelveygreg <mckelveygreg@gmail.com>
2018-07-10 20:28:43 -0700

    Subheading links/list

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var navbar = document.querySelector('#navbar');
 
 sections.forEach(section => {
     var header = section.firstElementChild.textContent;
-    var sectionId = header.replace(/\s+/g, '_');
+    let sectionId = header.replace(/\s+/g, '_');
     
     section.setAttribute('id', sectionId);
 
@@ -13,8 +13,32 @@ sections.forEach(section => {
     link.setAttribute('class', 'nav-link');
     
     link.innerText = header;
+
+    // Build subHeadings for each section
+    var subHeadings = section.querySelectorAll('h3');
+    // returns an unordered list of subheading links
+    subHeadings = buildSubHeadings(subHeadings);
+
     navbar.appendChild(link);
-})
+    navbar.appendChild(subHeadings);
+});
+
+function buildSubHeadings (el) {
+    var subUl = document.createElement('ul');
+    // Find each h3 element in each given section, make a list full of links
+    el.forEach(h3 => {
+        var subLi = document.createElement('li');
+        var subA = document.createElement('a');
+        let sectionId = h3.textContent.replace(/\s+/g, '_');
+        
+        subA.innerText = h3.textContent;
+        subA.setAttribute('href', '#' + sectionId);
+        subLi.appendChild(subA);
+        subUl.appendChild(subLi);
+    });
+
+    return subUl;
+}
 
 // Footer source url generator
 
